967d3308878f4ac92a61af104a116f1d6bb29a3f
jeckajeckovich <terekhin.evgeny.e@gmail.com>
2026-01-19 22:36:17 +0100

    Create flights.js

---
+++ flights.js
@@ -0,0 +1,22 @@
+const FLIGHTS = [
+  {
+    date: "19.01.2026",
+    origin: "DYR",
+    destination: "PWE",
+    flight_number: "21",
+    departure: "10:00",
+    arrival: "12:20",
+    aircraft: "ANTONOV AN-24",
+    price: 17000
+  },
+  {
+    date: "20.01.2026",
+    origin: "DYR",
+    destination: "PVS",
+    flight_number: "113",
+    departure: "10:00",
+    arrival: "12:00",
+    aircraft: "ANTONOV AN-24",
+    price: 15000
+  }
+];
